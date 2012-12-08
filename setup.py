470d2f7740985eda9431c7902b0bc590c3e326be
Gregory Nicholas <faction.gregory@gmail.com>
2012-12-07 23:14:13 -0800

    moved everything up a level from the flaskext/ folder

--- setup.py
+++ setup.py
@@ -16,28 +16,31 @@ from setuptools import setup
 
 
 setup(
-    name='Flask App Engine Upload',
-    version='1.0.0',
-    url='http://github.com/gregorynicholas/flask-gae_upload',
-    license='BSD',
-    author='gregorynicholas',
-    description='Flask extension module for working with file upload on App Engine.',
-    long_description=__doc__,
-    packages=['flaskext'],
-    namespace_packages=['flaskext'],
-    zip_safe=False,
-    platforms='any',
-    install_requires=[
-        'Flask'
-    ],
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-        'Topic :: Software Development :: Libraries :: Python Modules'
-    ]
+  name='Flask App Engine Upload',
+  version='1.0.0',
+  url='http://github.com/gregorynicholas/flask-gae_upload',
+  license='BSD',
+  author='gregorynicholas',
+  description='Flask extension module for working with file upload on App Engine.',
+  long_description=__doc__,
+  modules=[
+    'gae_deploy_tests',
+    'gae_deploy'],
+  # packages=['flaskext'],
+  # namespace_packages=['flaskext'],
+  zip_safe=False,
+  platforms='any',
+  install_requires=[
+    'Flask'
+  ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Environment :: Web Environment',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: BSD License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    'Topic :: Software Development :: Libraries :: Python Modules'
+  ]
 )
