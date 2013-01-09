c259fe13a838e1e7f50dbfff575eaaf52d523f11
Gregory Nicholas <faction.gregory@gmail.com>
2013-01-09 00:57:36 -0800

    added build_docs.sh script to generate sphinx docs.

---
+++ build_docs.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+cd ./docs
+make html
+cd ../
+
+git checkout gh-pages
+
+cp -rf ./docs/_build/html/* ./
+
+git add *.html searchindex.js objects.inv _static/ _sources/
+git commit -m "updated docs html build."
+git pull origin gh-pages
+git push origin gh-pages
+
+git checkout -f master
